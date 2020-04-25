7f4131b07411747f842e38102486d731b3808f06
Hunghh.Dev <hunghh.dev@gmail.com>
2020-04-25 20:20:18 +0700

    admin pages sorting

--- cmscart/routes/admin_pages.js
+++ cmscart/routes/admin_pages.js
@@ -9,8 +9,8 @@ var Page = require('../models/page')
  * Get page index
  */
 router.get('/', function (req, res) {
-    Page.find({}).sort({sorting: 1}).exec(function(err, pages){
-        res.render('admin/pages',{
+    Page.find({}).sort({ sorting: 1 }).exec(function (err, pages) {
+        res.render('admin/pages', {
             pages: pages
         })
     })
@@ -45,7 +45,7 @@ router.post('/add-page', [
         var content = req.body.content
 
         var result = validationResult(req)
-        if (result.errors.length>0) {
+        if (result.errors.length > 0) {
             res.render('admin/add_page', {
                 errors: result.errors,
                 title: title,
@@ -53,7 +53,7 @@ router.post('/add-page', [
                 content: content
             })
         } else {
-            Page.findOne({slug: slug}, function(err, page){
+            Page.findOne({ slug: slug }, function (err, page) {
                 if (page) {
                     req.flash('danger', 'Page slug exists. choose another')
                     res.render('admin/add_page', {
@@ -63,23 +63,42 @@ router.post('/add-page', [
                     })
                 } else {
                     var page = new Page({
-                        title: title, 
+                        title: title,
                         slug: slug,
                         content: content,
                         sorting: 100
                     })
 
-                    page.save(function(err){
-                        if(err) return console.log(err)
+                    page.save(function (err) {
+                        if (err) return console.log(err)
                         req.flash('success', 'Page added')
                         res.redirect('/admin/pages')
                     })
-                     
+
                 }
             })
         }
 
-    })
+    }
+)
 
+/*
+ * Post reorder pages
+ */
+router.post('/reorder-pages', function (req, res) {
+    var ids = req.body['id[]']
+    var count = 0
+    for (var i = 0; i < ids.length; i++) {
+        var id = ids[i]
+        count++
+
+        (function (count) {
+            Page.findById(id, function (err, page) {
+                page.sorting = count
+                page.save()
+            })
+        })(count)
+    }
+})
 
 module.exports = router
